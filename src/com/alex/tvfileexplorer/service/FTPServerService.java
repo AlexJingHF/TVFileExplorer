beaeee94342f8d3401e871b33d7e26e1ed4014ef
JingHaifeng <huq33@126.com>
2014-08-17 22:06:17 +0800

    Add some files

---
+++ src/com/alex/tvfileexplorer/service/FTPServerService.java
@@ -0,0 +1,53 @@
+package com.alex.tvfileexplorer.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * Created by alex on 14-8-16.
+ */
+public class FTPServerService extends Service implements Runnable {
+    private static final String TAG = FTPServerService.class.getSimpleName();
+
+    protected static Thread SERVER_THREAD = null;
+
+    public FTPServerService(){
+
+    }
+
+    /**
+     * Starts executing the active part of the class' code. This method is
+     * called when a thread is started that has been created with a class which
+     * implements {@code Runnable}.
+     */
+    @Override
+    public void run() {
+
+    }
+
+    /**
+     * Return the communication channel to the service.  May return null if
+     * clients can not bind to the service.  The returned
+     * {@link android.os.IBinder} is usually for a complex interface
+     * that has been <a href="{@docRoot}guide/components/aidl.html">described using
+     * aidl</a>.
+     * <p/>
+     * <p><em>Note that unlike other application components, calls on to the
+     * IBinder interface returned here may not happen on the main thread
+     * of the process</em>.  More information about the main thread can be found in
+     * <a href="{@docRoot}guide/topics/fundamentals/processes-and-threads.html">Processes and
+     * Threads</a>.</p>
+     *
+     * @param intent The Intent that was used to bind to this service,
+     *               as given to {@link android.content.Context#bindService
+     *               Context.bindService}.  Note that any extras that were included with
+     *               the Intent at that point will <em>not</em> be seen here.
+     * @return Return an IBinder through which clients can call on to the
+     * service.
+     */
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
